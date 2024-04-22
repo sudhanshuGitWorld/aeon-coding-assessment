eb6dcead8e96507f6ccddbc0d503875c40b372bc
SUDHANSHU SHRIVASTAVA <sudhanshumac@Sudhanshus-MacBook-Pro.local>
2024-04-22 17:42:35 +0530

    Navbar development done with desktop, tablet & mobile views

--- src/app/navbar/page.js
+++ src/app/navbar/page.js
@@ -1,16 +1,59 @@
-'use client'
-import Footer from '../components/footer';
+"use client";
+import { useRef, useState } from "react";
+import Link from "next/link";
+import { FaBars, FaTimes, FaSearch } from "react-icons/fa";
+import Footer from "../components/footer";
 
 const Navbar = () => {
+  const navRef = useRef();
+  const [isMobileView, setMobileView] = useState(true);
 
-    return (
-        <>
-            <div className='container'>
-                <h2>This is a navbar page</h2>
-            </div>
-            <Footer prevPagePath={'/calculator'} nextPagePath={'/findTargetIndexes'} isNextBtnShow isPrevBtnShow />
-        </>
-    );
-}
- 
+  const menuList = ['Showcase', 'Docs', 'Blog', 'Analytics', 'Templates', 'Enterprise'];
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+    setMobileView(!isMobileView);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <header>
+          <h4>AEON</h4>
+          <nav ref={navRef}>
+            {menuList?.map((menu, idx) => (
+                <Link href='/' key={idx}>{menu}</Link>
+                // we should not use index as a key. But here, I don't have any id so for task only, I used it.
+            ))}
+            <button className="nav-search-icon">
+                <FaSearch />
+            </button>
+            <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+              <FaTimes />
+            </button>
+          </nav>
+          <div className="search">
+            <input
+              type="search"
+              name="search"
+              placeholder="Search documentation..."
+            />
+          </div>
+          {isMobileView && (
+            <button className="nav-btn" onClick={showNavbar}>
+              <FaBars />
+            </button>
+          )}
+        </header>
+      </div>
+      <Footer
+        isNextBtnShow
+        isPrevBtnShow
+        prevPagePath={"/calculator"}
+        nextPagePath={"/findTargetIndexes"}
+      />
+    </>
+  );
+};
+
 export default Navbar;
