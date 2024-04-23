a9d6528081f8562db127c4fbeb217e47472cc6ee
SUDHANSHU SHRIVASTAVA <sudhanshumac@Sudhanshus-MacBook-Pro.local>
2024-04-23 10:38:27 +0530

    Two sum input array functionality done

---
+++ src/app/findTargetIndexes/target.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+
+export const twoSum = function (numbers, target) {
+        let left = 0;
+        let right = numbers?.length - 1;
+        while(left < right) {
+            const sum = numbers[left] + numbers[right];
+            if(sum === target) return [left+1, right+1]
+            sum > target ? right-- : left++
+        }
+};
